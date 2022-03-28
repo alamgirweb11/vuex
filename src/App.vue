0175d20b33059f27ecba9fce1d317ca35a25e34e
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-28 23:41:25 +0600

    store and use it to hold state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
       <div class="container">
          <h3 class="msg">{{ msg }}</h3>
+         <br>
+         <h2 :class="[{ 'text-black':count === 0}, {'text-success' : count>0},  {'text-danger' : count<0}]">{{ count }}</h2>
+          <button class="increment-btn" @click="increment()">+</button>  <button class="decrement-btn" @click="decrement()">-</button>
       </div>
 </template>
 
@@ -11,12 +14,56 @@ export default {
   data(){
        return{
           msg: this.$store.state.msg,
+          count: this.$store.state.count,
        }
   },
+  methods:{
+         increment(){
+             return this.count++
+         },
+         decrement(){
+              return this.count-- 
+         }
+  }
 }
 </script>
 <style scoped>
+  .container{
+      width: 80%;
+      margin: 0 auto;
+  }
   .msg{
-     text-align: center;
+     text-align: left;
+  }
+  .text-gray{
+       color: #000;
+  }
+  .text-success{
+      color: #15ec6ff5;;
+  }
+  .text-danger{
+      color: #f05902f5;
+  }
+  .increment-btn{
+        background-color: #15ec6ff5;
+        color: #fff;
+        padding: 5px 10px;
+        border: none;
+        outline: none;
+        font-size: 1.3rem;
+        font-weight: bold;
+        cursor: pointer;
+        margin: 0 10px;
+  }
+  .decrement-btn{
+        background-color: #f05902f5;
+        color: #fff;
+        padding: 5px 10px;
+        border: none;
+        outline: none;
+        font-size: 1.3rem;
+        font-weight: bold;
+        cursor: pointer;
+        margin: 0 10px;
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,6 @@ import { createStore } from 'vuex'
 export default createStore({
     state: {
         msg: 'Hello World!',
+        count: 0,
     },
 })

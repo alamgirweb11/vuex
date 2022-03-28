054527f2930cbfa074ca848c19f40d922768057a
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-28 20:42:03 +0600

    vuex init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container">
+         <h3 class="msg">{{ msg }}</h3>
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+       return{
+          msg: this.$store.state.msg,
+       }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .msg{
+     text-align: center;
+  }
 </style>
